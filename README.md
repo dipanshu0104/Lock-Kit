a75e5387cf0deb26e0b609779860b33f6ea7d17d
dipanshu0104 <dipanshu.kumar0025@gmail.com>
2023-08-26 08:17:12 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+
+# LOCK-KIT 
+Change Your WIndows pc password without any pervious pasword.
+
+# What is LOCK-KIT? 
+
+The LOCK-KIT tool is basically used to set or reset the Windows users password without knowing any previous password. 
+
+## Authors
+
+- [Its-Jerry](https://github.com/Its-Jerry)
+
+## Screenshot
+Screenshot 1.
+  ![username](https://user-images.githubusercontent.com/126962359/227423038-6fc896da-a2a4-43fa-8916-0e795e18433c.PNG)
+
+Screenshot 2.
+
+![helpsection](https://user-images.githubusercontent.com/126962359/227423118-121d3584-675a-47a9-aa8c-e05efe13e4f7.PNG)
+
+## Features
+
+ - User friendly interface.
+ - Reset multiple users password.
+ - Check the list of users avilable in your system.
+ - Easily see the details about the the users like ( type of Account ).
+
+## Installation
+
+- Step 1. Clone the code by git clone. 
+
+```bash
+git clone https://github.com/Its-Jerry/Lock-Kit.git 
+```
+   ( You can also download the zip file directly. ) 
+
+- Step 2. You can see the lock-kit.bat file. Right click
+        on it. And Run it As Adminstrator mode. And use it.
+
+- Step 3. If you want help about the tool.
+so you Press 3 and then Press Enter. You see a help section. You can read it for help.
+
+    
+
+
+## Testing 
+
+This tool is tested on :
+
+- Windows 7
+- Windows 8
+- Windows 8.1
+- Windows 10 ( All Versions )
+- Windows 11 ( All versions )
+
+
+###    I hope you will use this tool very efficiently.
---
+++ lock-kit.bat
@@ -0,0 +1,190 @@
+@echo off
+title Lock-Kit( change pc password without any pervious pasword. )
+color 0A
+cls
+echo.
+echo.
+echo     Loading [}                                                 ] 0%%
+ping localhost -n 2 >nul
+
+cls
+echo.
+echo.
+echo     Loading [#####}                                            ] 10%%
+ping localhost -n 2 >nul
+
+cls
+@REM color 6
+echo.
+echo.
+echo     Loading [##########}                                       ] 20%%
+ping localhost -n 2 >nul
+
+cls
+echo.
+echo.
+echo     Loading [###############}                                  ] 30%%
+ping localhost -n 2 >nul
+
+cls
+echo.
+echo.
+echo     Loading [####################}                             ] 40%%
+ping localhost -n 2 >nul
+
+cls
+@REM color B
+echo.
+echo.
+echo     Loading [#########################}                        ] 50%%
+ping localhost -n 2 >nul
+
+cls
+echo.
+echo.
+echo     Loading [##############################}                   ] 60%%
+ping localhost -n 2 >nul
+
+cls
+echo.
+echo.
+echo     Loading [###################################}              ] 70%%
+ping localhost -n 2 >nul
+
+cls
+@REM color 9
+echo.
+echo.
+echo     Loading [########################################}         ] 80%%
+ping localhost -n 2 >nul
+
+cls
+echo.
+echo.
+echo     Loading [#############################################}    ] 90%%
+ping localhost -n 2 >nul
+
+cls
+@REM color A
+echo.
+echo.
+echo     Loading [##################################################] 100%%
+ping localhost -n 2 >nul
+
+cls
+echo.
+echo.
+echo     Loading [                    Complete                      ] 100%%
+ping localhost -n 3 >nul
+
+cls
+:loop
+color 0A
+echo      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo      "                                                                              "
+echo      "             .^!7777777~:.                 Date : %date%              "                   
+echo      "           !5B&##BBBBB###BY~                                                  "                   
+echo      "         :P&&5           P&#Y.                                                "                   
+echo      "        :B&#!             ?#&5                                   _            "                   
+echo      "        7&&J               P##^       +-+       _____    _____  | |  __       "                   
+echo      "        ?&&?               5##~       | |      /  _  \  / ____) | | / _)      "                   
+echo      "        ?&&?               5##~       | |      | | | | { |      | |/ /        "                   
+echo      "      ^JG##G555555555555555B##P?^     | |____  | |_| | { |____  | |^\ \       "                   
+echo      "     ~#&#########&&&&&#########&#^    \______) \_____/  \_____) |_|  \_\      "                   
+echo      "     ?&##########BP5PB##########&!                                            "                   
+echo      "     ?&#########Y     !B########&!                                            "                   
+echo      "     ?&#########.      5########&!         _         _____    _______         "                   
+echo      "     ?&#########P~.  :J#########&!        | |  __   {_   _}  (__   __)        "                   
+echo      "     ?&##########&!  G&#########&!        | | / _)    | |       | |           "                   
+echo      "     ?&##########&!  G##########&!        | |/ /      | |       | |           "                   
+echo      "     ?&###########GJ5###########&!        | |^\ \    _| |_      | |           "                   
+echo      "     7&############&&###########&~        |_|  \_\  {_____}     |_|           "                   
+echo      "     .JB#######################BJ                                             "                   
+echo      "       .^^^^^^^^^^^^^^^^^^^^^^:.                                              "
+echo      "                                     Author : Its-Jerry                       "
+echo      "                                     Github : https://github.com/Its-Jerry    "
+echo      "                                                                              "
+echo      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo.
+echo.  
+echo        **********Operation**********
+echo.
+
+echo         1) Find Username.
+echo         2) Change Password.
+echo         3) Help to use the tool.
+echo         4) Exit.
+echo.
+       set /p choice=   Enter operation number : 
+       if not defined choice goto empty 
+       if %choice% == 1 ( goto op1 ) 
+       if %choice% == 2 ( goto op2 ) 
+       if %choice% == 3 ( goto op3 ) 
+       if %choice% == 4 ( goto op4 ) else ( goto not )
+    
+:op1
+net user 
+pause
+echo.
+set choice=
+cls
+goto loop
+
+:op2
+set /p user= Enter the Username : 
+net user %user% *
+pause
+echo.
+set choice=
+cls
+goto loop
+
+:op3
+color 06
+echo.
+echo      Step 1.  Find your Username by press 1. ( If you don't know.)
+echo               Username is exist in Administrator column.
+echo.
+echo      Step 2.  press 2 to change the password.
+echo.
+echo      Step 3.  Enter your Username.
+echo.
+echo      Step 4.  Enter the new password for the user of your choice. And
+echo               retype to confirm it.
+echo.
+echo      Step 5.  your password is changed. Now you can exit by press 4.
+echo.
+pause
+set choice=
+cls
+goto loop
+
+:op4 
+color 09
+echo.
+echo       Exiting.........
+ping localhost -n 4 >nul
+set choice=
+exit
+
+:empty
+color 07
+echo. 
+echo   Please enter any operation to run the tool.
+ping localhost -n 4 >nul
+set choice=
+cls
+goto loop
+
+:not 
+color 0C
+echo.
+echo    ! Invalid operation. please try again.
+ping localhost -n 4 >nul
+set choice=
+cls
+goto loop
+
+      
+       
+  
